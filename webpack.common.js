7ffcb079dc85fd0a470da76a305e65bdeebd41f4
Biryukova Nadezhda <biryukova.nadezhda@bk.ru>
2022-11-03 01:47:47 +0300

    Fix header and section-trends styles

--- webpack.common.js
+++ webpack.common.js
@@ -57,6 +57,7 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html',
+      favicon: './src/favicon.ico'
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
